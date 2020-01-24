7a237d6fe619e65d335e4dea4b81ebc7b3674ffa
Jacob Martinez <jmartinez.data@gmail.com>
2020-01-24 11:51:39 -0600

    Finalized Add method

--- CustomList/CustomList/CustomList.cs
+++ CustomList/CustomList/CustomList.cs
@@ -4,22 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CustomList
+namespace CList
 {
     public class CustomList<T>
     {
-        T[] items;
-        readonly public int Count;
-
+        public T[] array;
+        public int Count;  //needs to be read only
+        public int Capacity; // { get; }; needs to be read only
+        
+        public T this[int i]  // indexer
+        {
+            get => array[i];
+            set => array[i] = value;
+        }
+        
         // Can use .Equals()
         public CustomList()
         {
-
+            Capacity = 4;
+            Count = 0;
+            array = new T[Capacity];
         }
 
-        public void Add(T item)
+        public void Add(T newItem)        
         {
-
+            if (Count == Capacity)
+            {
+                Capacity += 4;
+                T[] temporary = array;
+                array = new T[Capacity];
+                for (int i = 0; i < Count; i++)
+                {
+                    array[i] = temporary[i];
+                }                
+            }
+            array[Count++] = newItem;
         }
         public void Remove(T item)
         {
--- CustomList/CustomList/Program.cs
+++ CustomList/CustomList/Program.cs
@@ -4,12 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CustomList
+namespace CList
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Testing methods and properties
+            CustomList<int> testList = new CustomList<int>();
+
+            Console.WriteLine("The Capacity is: {0}", testList.Capacity);
+            Console.WriteLine("The Count is: {0}\n", testList.Count);
+            testList.Add(5);
+            testList.Add(3);
+            testList.Add(4);
+            testList.Add(6);
+            testList.Add(1);
+            Console.WriteLine("The Capacity is: {0}", testList.Capacity);
+            Console.WriteLine("The Count is: {0}\n", testList.Count);
+            for (int i = 0; i < testList.Count; i++)
+            {
+                Console.WriteLine(testList[i]);
+            }            
+            Console.Read();
+
         }
     }
 }
